32218b8ca1ddd3248f8276a85d255f61199663de
mamerida <mamerida2018@gmail.com>
2024-06-21 16:52:20 -0300

    complete dashboard page

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import {Montserrat} from 'next/font/google'
+import {Montserrat , Lusitana} from 'next/font/google'
 //export fonts tu use in my proyect
 export const monserrant = Montserrat({subsets:['latin'], weight:['400','500','600','700']})
+export const lusitana = Lusitana({subsets:['latin'], weight:["400","700"]})
